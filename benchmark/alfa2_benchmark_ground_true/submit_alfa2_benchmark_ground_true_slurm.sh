f685e703b0101aa454c7945c70629788121df5e2
Li Tao <aeolia.zafiro@gmail.com>
2026-06-15 18:03:07 -0400

    Use available R 4.4 module for benchmark submitter

--- benchmark/alfa2_benchmark_ground_true/submit_alfa2_benchmark_ground_true_slurm.sh
+++ benchmark/alfa2_benchmark_ground_true/submit_alfa2_benchmark_ground_true_slurm.sh
@@ -149,7 +149,7 @@ ALFAKR_REPO="${ALFAKR_REPO:-$(cd "${ALFAK2_REPO}/.." && pwd)/alfakR}"
 OUTPUT_DIR="${OUTPUT_DIR:-${ALFAK2_REPO}/benchmark/results/alfa2_benchmark_ground_true}"
 RUNNER="${RUNNER:-${ALFAK2_REPO}/benchmark/alfa2_benchmark_ground_true/run_alfa2_benchmark_ground_true.R}"
 R_BIN="${R_BIN:-Rscript}"
-MODULES="${MODULES-R/4.4}"
+MODULES="${MODULES-R/4.4.0}"
 
 SAMPLE_DEPTHS="${SAMPLE_DEPTHS:-1000,200}"
 WAVELENGTHS="${WAVELENGTHS:-0.2,0.4,0.8,1.6}"
